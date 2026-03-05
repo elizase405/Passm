c069d4372810eec95b873cf1b807a6762bf36a8a
Elizabeth <elizase405@gmail.com>
2026-03-05 18:40:49 +0100

    fix: removed useAuth in protectedRoute

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { useAuth } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
-    const { user } = useAuth();
+    const { user } = null
 
     if (user === null) {
         return <p>Loading...</p>;
