3a6076ffb98ad9be0699052b9337d8aafa1f321d
Elizabeth <elizase405@gmail.com>
2026-04-11 10:16:26 +0100

    fix: using local backend with deployed frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://passm-ajf4.vercel.app/api",
+  baseURL: "http://localhost:3000/api",
   withCredentials: true, // IMPORTANT to send/receive cookies
 });
 
