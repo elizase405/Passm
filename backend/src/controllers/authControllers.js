646cf05e34e8c6eca7984a0be51ad5c750e058b4
Elizabeth <elizase405@gmail.com>
2026-04-11 09:45:51 +0100

    fix: checking if token is passed to cookies

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -49,6 +49,7 @@ const login = async (req, res) => {
 
 const getAuthenticatedUser = async (req, res) => {
     const token = req.cookies.token;
+    console.log("Cookies: ", req.cookies);
     console.log("Get token: ", token);
     if (!token) return res.status(401).json({ message: "Unauthorized" });
     try {
